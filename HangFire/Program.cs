5d2f55056b8d870f8459d7ff46b63ebf6deb7ed7
t1.kashehi <kashehi2003@gmail.com>
2024-02-12 11:47:12 +0330

    Add Swagger and Route

--- HangFire/Program.cs
+++ HangFire/Program.cs
@@ -1,6 +1,29 @@
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();  
+builder.Services.AddSwaggerGen();
+builder.Services.AddHangfire(configuration: cnf =>
+{
+    cnf.UseSqlServerStorage(nameOrConnectionString: builder.Configuration["ConnectionStrings:HangFireConnection"]);
+    cnf.UseSimpleAssemblyNameTypeSerializer();
+    cnf.UseRecommendedSerializerSettings();
+});
+
+builder.Services.AddHangfireServer();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUi();
+}
+app.UseHttpsRedirection();
+app.MapControllers();
+//app.MapGet("/", () => "Hello World!");
+app.UseHangfireDashboard(pathMatch: "/dashboard");
 app.Run();
